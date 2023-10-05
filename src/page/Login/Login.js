b8664ff70bd64b856f6fa526712a58f7330b62ff
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2023-10-05 14:02:05 -0300

    Add files via upload

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,85 +1,97 @@
 import React from 'react'
 import style from "../Login/Login.module.css"
-import { useState,useEffect } from 'react'
-import { Link,useNavigate} from "react-router-dom";
+
+
+import { useState, useEffect } from 'react'
+import { Link, useNavigate } from "react-router-dom";
 import { useAuthentication } from '../../Hooks/useAuthentication'
 
 import { useAuthValue } from '../../Context/authContext';
 
 const Login = () => {
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const [error,setError] = useState()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState()
 
-  const {login, error: authError , loading,redirect,GoogleLogar} = useAuthentication()
+  const { login, error: authError, loading, redirect, GoogleLogar } = useAuthentication()
 
   const navigate = useNavigate()
-  const {user:contextUser} = useAuthValue()
+  const { user: contextUser } = useAuthValue()
 
-  const handleSubmit =  async (e)=>{
- e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
- setError('')
+    setError('')
 
- const user = {
-  email,
-password
- }
+    const user = {
+      email,
+      password
+    }
 
- const res = await login(user)
+    const res = await login(user)
   }
 
 
-  useEffect(()=>{
-    if (contextUser && redirect === 1 &&  contextUser.emailVerified  === true     ) {
+  useEffect(() => {
+    if (contextUser && redirect === 1 && contextUser.emailVerified === true) {
       navigate("/")
 
     }
-    else  if  (contextUser && contextUser.emailVerified  === false) {
-   setError(' Confirme seu email por favor! ')
-    } 
-  
-   },[redirect])
+    else if (contextUser && contextUser.emailVerified === false) {
+      console.log('Confirme seu email antes por favor')
+    }
+
+  }, [redirect])
 
 
-console.log(contextUser)
-  useEffect(()=>{
-    if(authError){
+  useEffect(() => {
+    if (authError) {
       setError(authError)
     }
   })
   return (
     <div className={style.LoginMain}>
-           <div className={style.LoginForm}>
-            <h3>Entrar</h3>
-           <form  onSubmit={handleSubmit}>
-            <label >
-              Email
-              <input
-               type="email"
-                name='email' 
-                required
-                value={email}
-                onChange={(e)=>{
-              setEmail(e.target.value)
-              }}/>
-            </label>
-            <label >
-              Senha
-              <input 
-              type="password" 
-              name='password' 
+      <div className={style.LoginForm}>
+        <h3>Entrar</h3>
+        <form onSubmit={handleSubmit}>
+          <label >
+            Email:
+            <input
+              type="email"
+              name='email'
               required
-              value={password} 
-               onChange={(e)=>{setPassword(e.target.value)}}/>
-            </label>
-            <hr/>
-            <input type="submit" value="Logar" name='logar' />
-         </form>
-         {error && <p>{error}</p>}
-          <p>Não tem uma conta?<span><Link to ='/cadastre-se'>Crie uma</Link></span></p>
-          <p>Esqueci minha senha</p>
-           </div>
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }} />
+          </label>
+
+
+          <label >
+            <div>
+              Senha:
+            </div>
+            <input
+              type="password"
+              name='password'
+              required
+              value={password}
+              onChange={(e) => { setPassword(e.target.value) }} />
+               <Link to='#'><p>Esqueci minha senha</p></Link>
+          </label>
+
+          <hr />
+
+        </form>
+
+
+        <div>
+          <input type="submit" value="Entrar agora" name='logar' />
+          {error && <p>{error}</p>}
+          <p>Não tem uma conta?<span><Link to='/cadastre-se'>Crie uma</Link></span></p>
+        </div>
+
+      </div>
     </div>
   )
 }
